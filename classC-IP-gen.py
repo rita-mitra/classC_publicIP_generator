7926bdc1e83012c6aa893fab80cd43a7217dc187
rita-mitra <45219828+rita-mitra@users.noreply.github.com>
2024-02-04 13:50:57 -0600

    Add "classC-IP-gen.py

---
+++ classC-IP-gen.py
@@ -0,0 +1,12 @@
+import random
+
+def generate_class_c_ip():
+    first_octet = random.randint(192, 223)
+    second_octet = random.choice([x for x in range(256) if x != 168])
+    third_octet = random.randint(0, 255)
+    fourth_octet = random.randint(0, 254)
+
+    return f"{first_octet}.{second_octet}.{third_octet}.{fourth_octet}"
+
+random_ip = generate_class_c_ip()
+print("Random Class C Public IP address:", random_ip)
